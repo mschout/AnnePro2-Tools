e2e24b0e5152584b0c81c924b769b4e1f7e07052
Michael Schout <schoutm@gmail.com>
2023-11-28 13:44:47 -0600

    Fix compilation on Linux

--- src/annepro2.rs
+++ src/annepro2.rs
@@ -105,11 +105,9 @@ pub fn flash_firmware<R: std::io::Read>(
 fn fetch_devices(api: &HidApi) -> (Vec<&hidapi::DeviceInfo>, Option<&hidapi::DeviceInfo>) {
     for dev in api.device_list() {
         println!(
-            "HID Dev: {:04x}:{:04x} usage #: {:02x} usage_page #: {:04x} {}",
+            "HID Dev: {:04x}:{:04x} {}",
             dev.vendor_id(),
             dev.product_id(),
-            dev.usage(),
-            dev.usage_page(),
             dev.product_string()
                 .map(|it| format!("({:})", it.replace("\n", " - ")))
                 .unwrap_or_default()
